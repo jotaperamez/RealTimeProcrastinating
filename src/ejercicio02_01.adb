680f0ac4d1ffad3b399248374025fe5798025595
Javi <lord.darkuness@gmail.com>
2018-03-04 13:55:08 +0100

    Subida ejercicio 2 completo

---
+++ src/ejercicio02_01.adb
@@ -0,0 +1,23 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Ejercicio02_01 is
+   
+   str:String(1..1);
+   Entero:Integer;
+   char:Character;
+begin
+   
+   loop
+      Get_Line(Str,Entero);
+      char:= str(1);
+      exit when Char = '0';
+      case Char is
+         when 'A'|'B' => Put_Line("Opcion 1");
+         when 'C'|'D'|'E' => Put_Line("Opcion 2");
+         when 'F' => Put_Line("Opcion 3");
+        when others => Put_Line("Otra opcion");   
+      end case;
+      
+   end loop;
+      
+end ejercicio02_01;
